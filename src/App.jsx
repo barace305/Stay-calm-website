eec40d53f07f02f3ccf4875d92541611ff512af4
barace305 <barace305@gmail.com>
2026-06-01 23:15:31 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import HelpFunnel from './pages/HelpFunnel';
+import Chiropractor from './pages/Chiropractor';
+import Towing from './pages/Towing';
+import BodyShop from './pages/BodyShop';
 
 export default function App() {
   return (
@@ -8,6 +11,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/help" element={<HelpFunnel />} />
+        <Route path="/chiropractor" element={<Chiropractor />} />
+        <Route path="/towing" element={<Towing />} />
+        <Route path="/body-shop" element={<BodyShop />} />
       </Routes>
     </Router>
   );
